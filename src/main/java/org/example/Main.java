21c87e4ccf0983432e3a5475bb3ae6516948129b
Stefanos Michaelides <stefanos2291@gmail.com>
2025-06-01 18:47:14 +0100

    Add IntelliJ Gradle project files and source code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.Supplier.Product;
+import org.example.Supplier.Supplier;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         Supplier primark = new Supplier("Primark", "020111222", "primark@gmail.com", "22 Primark Avenue");
+            Product tshirt = new Product("tshirt", 12.99, 12);
+
+            primark.addProduct(tshirt);
+            System.out.println(primark.getName());
+            System.out.println(primark.getContactInfo(Supplier.ContactDetails.ADDRESS));
+            System.out.println(primark.getProductsList(0).getProductName() + " :: Product ID :: " + tshirt.getProductId());
+        }
     }
 }
